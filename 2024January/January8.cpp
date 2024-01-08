6c1214b8055c514d5665a5559e70e3e5eae3930d
jongman22 <whdgjs3131@naver.com>
2024-01-09 01:55:23 +0900

    DP fundamental

---
+++ 2024January/January8.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <cmath> //pow함수 선언
+#include <limits.h>
+
+#define MAX 20001
+using namespace std;
+#define INF 10000000
+
+// 1로 만들기
+// int dp[INF];
+// int main(){
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     int test;
+//     cin>>test;
+//     dp[1]=0;
+//     for(int i=2;i<=test;i++){
+//         dp[i]=dp[i-1]+1;
+//         if(i%3==0){
+//             dp[i]=min(dp[i],dp[i/3]+1);
+//         }
+//         if(i%2==0){
+//             dp[i]=min(dp[i],dp[i/2]+1);
+//         }
+//     }
+//     cout<<dp[test]<<"\n";
+//     return 0;
+
+// }
+
+// 1003번 피보나치 함수
+// int zero[41];
+// int one[41];
+// void fibo(int N){
+//     for(int j=2;j<=N;j++){
+//         zero[j]=zero[j-1]+zero[j-2];
+//         one[j]=one[j-1]+one[j-2];
+//     }
+// }
+
+// int main(){
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     int testcase;
+//     cin>>testcase;
+//     one[0]=0;
+//     one[1]=1;
+//     zero[0]=1;
+//     zero[1]=0;
+//     for(int i=0;i<testcase;i++){
+//         int N;
+//         cin>>N;
+//         fibo(N);
+//         cout<<zero[N]<<" "<<one[N]<<"\n";
+//     }
+//     return 0;
+// }
+
+int dp[20][20];
+int ans[20][20];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int testcase;
+    cin >> testcase;
+    for (int i = 0; i < testcase; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+        for (int j = 0; j <= n; j++)
+        {
+            for (int u = 1; u <= k; u++)
+            {
+
+                if (j == 0)
+                {
+                    dp[j][u] = u;
+                    ans[j][u] = u;
+                }
+                if (u != 1 && j != 0)
+                {
+                    dp[j][u] = dp[j - 1][u] + dp[j][u - 1];
+                    ans[j][u] += dp[j][u];
+                }
+            }
+        }
+        cout << ans[n][k] << "\n";
+    }
+}
